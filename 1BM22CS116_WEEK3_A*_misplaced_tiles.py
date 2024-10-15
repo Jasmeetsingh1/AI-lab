ba241904c607d2416c30f04974eeab900f259373
Jasmeetsingh1 <160738992+Jasmeetsingh1@users.noreply.github.com>
2024-10-15 12:52:57 +0530

    Update 1BM22CS116_WEEK3_A*_misplaced_tiles.py
    WEEK 3

--- 1BM22CS116_WEEK3_A*_misplaced_tiles.py
+++ 1BM22CS116_WEEK3_A*_misplaced_tiles.py
@@ -81,3 +81,35 @@ if solution_misplaced:
         print_state(step)
 else:
     print("No solution found using misplaced tiles heuristic.")
+
+
+
+
+
+
+--------------------------OUTPUT----------------------------------
+Solution using misplaced tiles heuristic:
+[2, 8, 3]
+[1, 6, 4]
+[7, 0, 5]
+
+[2, 8, 3]
+[1, 0, 4]
+[7, 6, 5]
+
+[2, 0, 3]
+[1, 8, 4]
+[7, 6, 5]
+
+[0, 2, 3]
+[1, 8, 4]
+[7, 6, 5]
+
+[1, 2, 3]
+[0, 8, 4]
+[7, 6, 5]
+
+[1, 2, 3]
+[8, 0, 4]
+[7, 6, 5]
+
